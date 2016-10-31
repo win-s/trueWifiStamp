adb994f42cf146657e96484e1c2c7ae65e2ec6b5
Asawin Sattiya <desperandro@gmail.com>
2016-10-31 17:32:32 +0700

    process transaction data

--- transactions.js
+++ transactions.js
@@ -1,17 +1,38 @@
 "use strict";
 var path = require("path");
 var moment = path.resolve(__dirname,"node_modules","moment","moment.js");
+var CONST = require("const.js");
 
 class transactions{
     constructor(transactionData,config){
         this._transactionData = transactionData;
         this._config = config;
+        this._duration = 0;
     }
     print(){
 
     }
     processData(){
-        
+        var startDate = null;
+        var cmd,date,stopDate,spentTime;
+        this._transactionData.forEach( (transaction)=>{
+            transaction = transaction.split(" ");
+            cmd = transaction[0];
+            date = transaction[1];
+
+            if(cmd === CONST.START){
+                startDate = moment( date,this._config.dateTimeFormat );
+            }else if(cmd === CONST.STOP){
+                if(startDate !== null){
+                    stopDate = moment( date, this._config.dateTimeFormat);
+                    spentTime = moment.duration( stopDate.diff(startDate) );
+                    this._duration += spentTime.minutes();
+                    this._duration += spentTime.seconds()
+
+                    startDate = null;
+                }
+            }
+        });
     }
     
 
